5cbc56ceb4afc4768b188b1bebb516c7ab74e141
Rostyk Podolets <ros.podolets@gmail.com>
2022-02-04 13:13:06 +0200

    redux-persist functional added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,38 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import rootReducer from './contacts/contacts-reduser';
 
-// console.log(rootReducer);
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+];
+
+const persistConfig = {
+  key: 'contacts',
+  storage,
+  blacklist: ['filter'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: {
-    contacts: rootReducer,
+    contacts: persistedReducer,
   },
+  middleware,
 });
+
+export const persistor = persistStore(store);
